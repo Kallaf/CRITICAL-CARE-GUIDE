0306f2e7fdd0960360290bb4201b1d91d240a688
Kallaf <kalaf1997@yahoo.com>
2020-02-25 02:00:28 +0200

    Use unfixed navbar

--- public/js/agitationScoring.js
+++ public/js/agitationScoring.js
@@ -1,42 +1,42 @@
 $(document).ready(function(){
 
     
-    document.getElementById("RSS-Results").innerHTML = "Click on the desired score in the above table to show it's result";
+    document.getElementById("RASS-Results").innerHTML = "\"Click on the desired score in the above table to show it's result\"";
     
-    function renderRSSResult(score,text)
+    function renderRASSResult(score,text)
     {
         var res = "<h3 class='h3-sub-title'>Score: "+score+"</h3><p>"+text+"</p>";
-        document.getElementById("RSS-Results").innerHTML = res;    
+        document.getElementById("RASS-Results").innerHTML = res;    
     }
 
     $("#RASS-pos-4").click(function(){
-        renderRSSResult("+4","Patients with a RASS of 2 to 4 are not sedated enough and should be assessed for pain, anxiety, or delirium. The underlying etiology of the agitation should be investigated and appropriately treated to achieve a RASS of -2 to 0.");
+        renderRASSResult("+4","Patients with a RASS of 2 to 4 are not sedated enough and should be assessed for pain, anxiety, or delirium. The underlying etiology of the agitation should be investigated and appropriately treated to achieve a RASS of -2 to 0.");
     });
     $("#RASS-pos-3").click(function(){
-        renderRSSResult("+3","Patients with a RASS of 2 to 4 are not sedated enough and should be assessed for pain, anxiety, or delirium. The underlying etiology of the agitation should be investigated and appropriately treated to achieve a RASS of -2 to 0.");
+        renderRASSResult("+3","Patients with a RASS of 2 to 4 are not sedated enough and should be assessed for pain, anxiety, or delirium. The underlying etiology of the agitation should be investigated and appropriately treated to achieve a RASS of -2 to 0.");
     });
     $("#RASS-pos-2").click(function(){
-        renderRSSResult("+2","Patients with a RASS of 2 to 4 are not sedated enough and should be assessed for pain, anxiety, or delirium. The underlying etiology of the agitation should be investigated and appropriately treated to achieve a RASS of -2 to 0.");
+        renderRASSResult("+2","Patients with a RASS of 2 to 4 are not sedated enough and should be assessed for pain, anxiety, or delirium. The underlying etiology of the agitation should be investigated and appropriately treated to achieve a RASS of -2 to 0.");
     });
     $("#RASS-pos-1").click(function(){
-        renderRSSResult("+1","Patients with a RASS of -2 to 1 are properly sedated.");
+        renderRASSResult("+1","Patients with a RASS of -2 to 1 are properly sedated.");
     });
     $("#RASS-0").click(function(){
-        renderRSSResult("0","Patients with a RASS of -2 to 1 are properly sedated.");
+        renderRASSResult("0","Patients with a RASS of -2 to 1 are properly sedated.");
     });
     $("#RASS-neg-1").click(function(){
-        renderRSSResult("-1","Patients with a RASS of -2 to 1 are properly sedated.");
+        renderRASSResult("-1","Patients with a RASS of -2 to 1 are properly sedated.");
     });
     $("#RASS-neg-2").click(function(){
-        renderRSSResult("-2","Patients with a RASS of -2 to 1 are properly sedated.");
+        renderRASSResult("-2","Patients with a RASS of -2 to 1 are properly sedated.");
     });
     $("#RASS-neg-3").click(function(){
-        renderRSSResult("-3","Patients with a RASS of ≤-3 should have their sedation decreased or modified in order to achieve a RASS of -2 to 0.");
+        renderRASSResult("-3","Patients with a RASS of ≤-3 should have their sedation decreased or modified in order to achieve a RASS of -2 to 0.");
     });
     $("#RASS-neg-4").click(function(){
-        renderRSSResult("-4","Patients with a RASS of ≤-3 should have their sedation decreased or modified in order to achieve a RASS of -2 to 0.");
+        renderRASSResult("-4","Patients with a RASS of ≤-3 should have their sedation decreased or modified in order to achieve a RASS of -2 to 0.");
     });
     $("#RASS-neg-5").click(function(){
-        renderRSSResult("-5","Patients with a RASS of ≤-3 should have their sedation decreased or modified in order to achieve a RASS of -2 to 0.");
+        renderRASSResult("-5","Patients with a RASS of ≤-3 should have their sedation decreased or modified in order to achieve a RASS of -2 to 0.");
     });
 });
